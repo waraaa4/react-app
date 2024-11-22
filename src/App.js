2e3974f638fc86e61218a20e94a2b60099053bb4
warcjh <warcjh00@naver.com>
2024-11-22 13:50:58 +0900

    s

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import Home from './features/Home';
-import Layout from './components/Layout';
-import BoardList from './features/BoardList';
-import BoardRegister from './features/BoardRegister';
-import Register from './features/Register';
-import BoardModify from './features/BoardModify';
-import BoardDetail from './features/BoardDetail';
-import Login from './features/Login';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./features/Home";
+import Layout from "./components/Layout";
+import BoardList from "./features/BoardList";
+import BoardRegister from "./features/BoardRegister";
+import Register from "./features/Register";
+import BoardModify from "./features/BoardModify";
+import BoardDetail from "./features/BoardDetail";
+import Login from "./features/Login";
 
 // npm install react-router-dom
 
@@ -24,15 +24,24 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Layout></Layout>}>
-          <Route path='/' element={<Home></Home>}></Route>
-          <Route path='/register' element={<Register></Register>}></Route>
-          <Route path='/login' element={<Login></Login>}></Route>
-          <Route path='/board/list' element={<BoardList></BoardList>}></Route>
-          <Route path='/board/register' element={<BoardRegister></BoardRegister>}></Route>
-          <Route path='/board/read/:no' element={<BoardDetail></BoardDetail>}></Route>
-          <Route path='/board/modify/:no' element={<BoardModify></BoardModify>}></Route>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/register" element={<Register></Register>}></Route>
+          <Route path="/login" element={<Login></Login>}></Route>
+          <Route path="/board/list" element={<BoardList></BoardList>}></Route>
+          <Route
+            path="/board/register"
+            element={<BoardRegister></BoardRegister>}
+          ></Route>
+          <Route
+            path="/board/read/:no"
+            element={<BoardDetail></BoardDetail>}
+          ></Route>
+          <Route
+            path="/board/modify/:no"
+            element={<BoardModify></BoardModify>}
+          ></Route>
         </Route>
-      </Routes >
+      </Routes>
     </>
   );
 }
